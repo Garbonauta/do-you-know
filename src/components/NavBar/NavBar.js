0377d429be52bfec77983618e2526f4791d07afd
Garbonauta <eduardo.moreno.j@gmail.com>
2018-04-11 21:14:14 -0500

    Logout popover was creating unnecessary Y overflow.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -14,26 +14,27 @@ import Typography from 'material-ui/Typography'
 import Avatar from 'material-ui/Avatar'
 import { Header, styles } from './Styles'
 
-function AvatarMenu (
-  {
-    pictureUrl, open, onClick, handleClose, logout,
-  }) {
+function AvatarMenu({ pictureUrl, open, onClick, handleClose, logout }) {
   return (
     <div>
       <Manager>
         <Target>
           <IconButton onClick={onClick}>
-            <Avatar src={pictureUrl} style={{cursor: 'pointer'}}/>
+            <Avatar src={pictureUrl} style={{ cursor: 'pointer' }} />
           </IconButton>
         </Target>
-        <Popper
-          placement='bottom-start'
-          eventsEnabled={open}>
+        <Popper placement="bottom-end" eventsEnabled={open}>
           <ClickAwayListener onClickAway={handleClose}>
-            <Grow in={open} id='menu-list-grow' style={{ transformOrigin: '0 0 0' }}>
+            <Grow
+              in={open}
+              id="menu-list-grow"
+              style={{ transformOrigin: '0 0 0' }}
+            >
               <Paper>
-                <MenuList role='menu'>
-                  <MenuItem button={true} onClick={logout}>{'Log Out'}</MenuItem>
+                <MenuList role="menu">
+                  <MenuItem button={true} onClick={logout}>
+                    {'Log Out'}
+                  </MenuItem>
                 </MenuList>
               </Paper>
             </Grow>
@@ -52,26 +53,30 @@ AvatarMenu.propTypes = {
   logout: PropTypes.func.isRequired,
 }
 
-function NavBar (
-  {
-    messages: {appName}, pictureUrl, iconAction,
-    avatarOpen, avatarClick, avatarRequestClose,
-    handleHome, logout, classes: {appBar, title},
-  }) {
+function NavBar({
+  messages: { appName },
+  pictureUrl,
+  iconAction,
+  avatarOpen,
+  avatarClick,
+  avatarRequestClose,
+  handleHome,
+  logout,
+  classes: { appBar, title },
+}) {
   return (
-    <AppBar position='absolute' className={appBar}>
+    <AppBar position="absolute" className={appBar}>
       <Toolbar>
-        <IconButton
-          color='inherit'
-          onClick={iconAction}>
-          <MenuIcon/>
+        <IconButton color="inherit" onClick={iconAction}>
+          <MenuIcon />
         </IconButton>
         <Header>
           <Typography
-            variant='title'
-            color='inherit'
+            variant="title"
+            color="inherit"
             onClick={handleHome}
-            className={title}>
+            className={title}
+          >
             {appName}
           </Typography>
         </Header>
@@ -80,7 +85,8 @@ function NavBar (
           open={avatarOpen}
           onClick={avatarClick}
           handleClose={avatarRequestClose}
-          logout={logout}/>
+          logout={logout}
+        />
       </Toolbar>
     </AppBar>
   )
